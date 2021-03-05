b62eb2f6f50376e5839a6a4c0c6ed6bbd8a57874
Nicholas Janita <nickjanita@gmail.com>
2021-03-05 18:34:06 -0500

    Base psuedo code and setups

--- AutoBar.ino
+++ AutoBar.ino
@@ -1,12 +1,26 @@
 //Auto bar Code
 //Created by Nick, Nate, Brennen 
+#include <Stepper.h>
 
 void setup() {
-  // put your setup code here, to run once:
-
+  //Serial setup
+  Serial.begin(9600);
+  
+  //Motor setup
+  Stepper stepper_NEMA17(STEPS_PER_REV, 8, 9, 10, 11);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() [
 
+  //Wait for serial
+
+  //Conveyor motor to drink choice
+
+  //Dispenser
+
+  //Conveyor back to zero
+
+  //Send done signal
+
+  //END LOOP
 }
